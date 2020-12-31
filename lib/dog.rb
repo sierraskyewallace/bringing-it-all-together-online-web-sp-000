4ff24e6612531e03b1bdf26e5906b8d3a73f0b02
Sierra Wallace <sskwallace@gmail.com>
2020-12-31 04:09:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,5 +36,7 @@ class Dog
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
 end 
