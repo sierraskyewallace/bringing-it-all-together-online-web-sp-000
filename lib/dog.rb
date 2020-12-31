c08f6904ff684d37b67d11a1c25702db00e32022
Sierra Wallace <sskwallace@gmail.com>
2020-12-31 03:49:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog 
-  attr_accessor 
+  attr_accessor :id, :name, :breed 
+  
+  def initialize(name, breed, id=nil)
+    @name = name
+    @breed = breed
+    @id = id 
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NONE EXISTS dogs (id )
 end 
