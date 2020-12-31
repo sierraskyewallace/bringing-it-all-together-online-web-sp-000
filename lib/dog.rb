1998a0a628bfa4235de6ced79a62d1a33d453f37
Sierra Wallace <sskwallace@gmail.com>
2020-12-31 03:50:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,5 +10,11 @@ class Dog
   def self.create_table
     sql = <<-SQL 
     CREATE TABLE IF NOT EXISTS dogs (
-    id )
+    id INTEGER PRIMARY KEY, 
+    name TEXT,
+    breed TEXT)
+    SQL
+    DB[:conn].execute
+  end
+  
 end 
