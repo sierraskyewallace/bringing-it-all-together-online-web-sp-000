ea36aa5ac4fdc008fe21dca7abd27412b5618fac
Sierra Wallace <sskwallace@gmail.com>
2020-12-31 04:29:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,16 @@ class Dog
     end
   end
   
-   def self.find_by_id(id)
+   def self.find_by_name(name, breed)
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name, breed).map do |row|
+      self.new_from_db(row)
+    end.first
  end
   
   
