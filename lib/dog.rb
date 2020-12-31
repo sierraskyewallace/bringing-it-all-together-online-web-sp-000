eea070a0250cd97b6bd6742c6e39bf3f9eb90e78
Sierra Wallace <sskwallace@gmail.com>
2020-12-31 04:30:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,10 @@ class Dog
   attr_reader :id
 
   
-  def initialize(name:, breed:, id: nil)
+  def initialize(id=nil, name, breed)
     @id = id
     @name = name
     @breed = breed
-
   end
   
   def self.create_table
